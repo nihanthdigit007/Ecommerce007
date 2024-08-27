15dc57bd5426a5798e49c55d2f6d70e19c3e0635
Nihanth Kalisetti <Nihanth.Kalisetti@godigit.com>
2024-08-27 12:14:31 +0530

    first commit

---
+++ src/main/java/com/digit/Ecommerce007/Ecommerce007Application.java
@@ -0,0 +1,13 @@
+package com.digit.Ecommerce007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce007Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/digit/Ecommerce007/Ecommerce007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digit.Ecommerce007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommerce007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
